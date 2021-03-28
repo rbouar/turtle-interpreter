0f3e7c61dcd5afd4fc69dbf537836a589e8a398f
BOUARAH Romain <romain.bouarah@etu.univ-paris-diderot.fr>
2021-03-28 22:39:38 +0200

    Add function to get string representation of an ast

--- src/ast.ml
+++ src/ast.ml
@@ -14,3 +14,32 @@ type instruction =
   | Assignation of variable * expression
 
 type programme = variable list * instruction list
+
+(* Fonctions pour afficher *)
+let rec expression_to_string expr =
+  match expr with
+  | Plus (l, r) -> "(" ^ expression_to_string l ^ " + " ^ expression_to_string r ^ ")"
+  | Moins (l,r) -> "(" ^ expression_to_string l ^ " - " ^ expression_to_string r ^ ")"
+  | Nombre n -> string_of_int n
+  | Var v -> v
+
+let rec instruction_to_string ins =
+  match ins with
+  | Avance expr -> "(Avance " ^ expression_to_string expr ^ ")\n"
+  | Tourne expr -> "(Tourne " ^ expression_to_string expr ^ ")\n"
+  | BasPinceau -> "(BasPinceau)\n"
+  | HautPinceau -> "(HautPinceau)\n"
+  | Assignation (v,e) -> "(" ^ v ^ " = " ^ expression_to_string e ^ ")\n"
+
+let rec programme_to_string (vl, il) =
+  variable_list_to_string vl ^ "\n" ^ instruction_list_to_string il
+  
+and variable_list_to_string vl =
+  match vl with
+  | [] -> "\n"
+  | v :: vl' -> v ^ "\n" ^ variable_list_to_string vl'
+
+and instruction_list_to_string il =
+  match il with
+  | [] -> ""
+  | i :: il' -> instruction_to_string i ^ instruction_list_to_string il'
